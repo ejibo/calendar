ea4a5058ee21c6c5049c3784e600de404d0b8392
annora <790722ce7803433aa7b95cc48827a5fd@huawei.com>
2019-04-10 14:21:06 +0800

    包含AppID，Account Sid，Auth Token，模板id 等信息的配置项

---
+++ application/extra/sms.php
@@ -0,0 +1,7 @@
+﻿<?php
+return [
+	'sms_appid' => '012e273529e240a1aa7e28cf29712084',		//应用id
+	'sms_accountsid' => '7bfb2be380412d06a4d4e4521b64429f',	//Account Sid
+	'sms_authtoken' => 'e456986f73262d935f6b5f9dec66c49c',	//Auth Token
+	'sms_templateid' => '453417',							//短信模板id
+];
